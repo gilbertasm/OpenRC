198e1e212ab7a05cf8353a767a0a24e6b20378c6
Gilbertas <gilbertasm@gmail.com>
2013-05-16 20:46:09 +0300

    Refactored script a bit: * Made script comply to 'modern perl' policies: using strict mode; three argument open(), etc; * Moved parts of code into separate into separate functions; * Changed some regular expressions and added comments to them * Commented some parts of code * Changed names of some variables, added whitespace, etc

--- scripts/lsb.pl
+++ scripts/lsb.pl
@@ -2,11 +2,14 @@
 # parse lsb header and print as in OpenRC style.
 # base code borrowed from update-rc.d of Debian
 
+use strict;
+use warnings;
 use Switch;
 use File::Basename;
 
 my $initdscript = shift;
 my $name = basename($initdscript);
+
 my %lsb2orc = ('$remote_fs' => 'netmount',
 	       '$local_fs' => 'localmount',
 	       'mountdevsubfs' => 'devfs',
@@ -18,91 +21,165 @@ my %lsb2orc = ('$remote_fs' => 'netmount',
 	       'mountkernfs' => 'procfs',
 	       '$portmap' => 'rpcbind');
 
-my %vsrv;
-open(VSERV, "</etc/insserv.conf") || die "error: unable to read /etc/insserv.conf";
-while (<VSERV>){
-    next if (m/^\#.*/);
-    chomp;
-    if (m/^(\$\w+)\s*(\S?.*)$/i) {
-	my $vs=$1;
-	foreach $srv (split(/ /,$2)) {
-	    if ($srv =~ m/\+(\S?.*)/i) {
-		$vsrv{$1} = $vs;
-	    }
-	}
-    }
-}
-
-my %lsbinfo;
 my $lsbheaders = "Provides|Required-Start|Required-Stop|Default-Start|Default-Stop|Short-Description|Description|Should-Start|Should-Stop|X-Start-Before|X-Start-After";
-open(INIT, "<$initdscript") || die "error: unable to read $initdscript";
-while (<INIT>) {
-    chomp;
-    $lsbinfo{'found'} = 1 if (m/^\#\#\# BEGIN INIT INFO\s*$/);
-    last if (m/\#\#\# END INIT INFO\s*$/);
-    if (m/^\# ($lsbheaders):\s*(\S?.*)$/i) {
-	$lsbinfo{lc($1)} = $2;
-    }
-}
-close(INIT);
 
-my $dep='';
-my $des='';
+
+
+# Usage      : parse_insservconf(configuratoin_file)
+# Purpose    : Parses insserv cinfiguration file into hash
+# Returns    : Reference to hash containing parsed insserv.conf info
+# Parameters : $file - filename of insserv configuration file
+# Comments   : Returned hash structure:
+#                dependency => facilitiy,
+#                dependency => facilitiy,
+#                etc
+#
+# TODO: handle insserv/overrides and insserv.conf.d directories
+
+
+sub parse_insservconf {
+  
+    my $file = shift;
+    my ($matched, $vs, $vsrv);
+    $vsrv = {};
+
+    open(my $vserv_fh, "<", $file) || die "error: unable to read $file";
+
+    while (<$vserv_fh>){
+        chomp;
+ 
+        # Skip comments
+        next if (m/^\#.*/);
+
+        $matched = m/
+                       ^           # start at string beginning
+                       (\$\w+)     # capture literal $ followed by word in $1
+                       \s+         # one or more whitespace characters
+                       (\S+.*)     # capture everything after whitespace in $2
+                   /ix;            # use case insensitive maching
+    
+        if ($matched) {
+	    $vs = $1;
+        
+	    foreach my $srv (split(/\s+/, $2)) {
+	        if ($srv =~ m/\+(\S+)/i) {
+		    $vsrv->{$1} = $vs;
+	        }
+	    }
+        }
+    }
+
+    return $vsrv
+}
+
+
+
+# Usage      : parse_initd_script(initd_script)
+# Purpose    : Parses init script LSB info  into hash
+# Returns    : Reference to hash containing parsed LSB info
+# Parameters : $initdscript - filename of initd script
+# Comments   : Function reads  LSB info from init script line by line
+#              and parses it into hash
+#              Returned hash structure:
+#               lsbheader => rest of line,
+#               lsbheader => rest of line,
+#               etc           
+
+sub parse_initd_script {
+
+    my $initdscript = shift;
+    my $lsbinforef  = {};
+    my $matched;
+
+    open(my $init_fh, "<", $initdscript) || die "error: unable to read $initdscript";
+
+    while (<$init_fh>) {
+        chomp;
+        $lsbinforef->{'found'} = 1 if (m/^\#\#\# BEGIN INIT INFO\s*$/);
+        last if (m/^\#\#\# END INIT INFO\s*$/);
+        $matched = m/
+                  ^              # match only  at string beginning
+                  \#\s           # literal # followed by one space character 
+                  ($lsbheaders): # capture $lsbheaders part in $1
+                  \s+            # skip whitespace 
+                  (\S+.*)$       # capture remaining of line in $2
+                  /xi;
+
+
+        if ($matched) {
+	    $lsbinforef->{lc($1)} = $2;
+        }
+    }
+
+    close($init_fh);
+    return $lsbinforef;
+}
+
+
 
 sub l2of {
     my @value;
     my $items = shift;
-    foreach $v (split(/ /, $items)) {
-	if($lsb2orc{$v}){
-	    $v=$lsb2orc{$v}
+    foreach my $item (split(/\s+/, $items)) {
+	if($lsb2orc{$item}){
+	    $item = $lsb2orc{$item}
 	}
-	push(@value,$v)
+	push(@value, $item)
     }
     return join(" ", @value)
 }
 
+my $dep='';
+my $des=''; 
+
+my $lsbinfo = parse_initd_script($initdscript);
+my $vsrv    = parse_insservconf("/etc/insserv.conf");
+
 # Check that all the required headers are present
-if ($lsbinfo{found}) {
-    foreach $key (keys %lsbinfo) {
+if ($lsbinfo->{found}) {
+    foreach my $key (keys %$lsbinfo) {
 	switch ($key) {
 	    case "provides" {
 		my @value;
-		foreach $v (split(/ /, $lsbinfo{$key})) {
-		    push(@value, $lsb2orc{$vsrv{$v}}) if($vsrv{$v});
-		    push(@value, $lsb2orc{$v}) if ($v);
-		    push(@value, $v) if ($v ne $name);
+		foreach my $item (split(/\s+/, $lsbinfo->{$key})) {
+		    push(@value, $lsb2orc{$vsrv->{$item}}) if($vsrv->{$item});
+		    push(@value, $lsb2orc{$item}) if ($item);
+		    push(@value, $item) if ($item ne $name);
 		}
 		if (@value) {
 		    $dep .= "\tprovide " . join(" ", @value) . "\n";
 		}
 	    }
 	    case "required-start" {
-		my $ret = l2of $lsbinfo{$key};
+		my $ret = l2of $lsbinfo->{$key};
 		if($ret) {$dep .= "\tneed " . $ret . "\n"}
 	    }
 	    case "required-stop" {}
 	    case "default-start" {}
 	    case "default-stop" {}
 	    case "short-description" {
-		$des .= 'description="'.$lsbinfo{$key}.'"'."\n";
+		$des .= 'description="'.$lsbinfo->{$key}.'"'."\n";
 	    }
 	    case "description" {}
 	    case "should-start" {
-		my $ret = l2of $lsbinfo{$key};
+		my $ret = l2of $lsbinfo->{$key};
 		if ($ret) {$dep .= "\tuse " . $ret . "\n"}
 	    }
 	    case "should-stop" {}
 	    case "x-start-before" {
-		my $ret = l2of $lsbinfo{$key};
+		my $ret = l2of $lsbinfo->{$key};
 		if ($ret) {$dep .= "\tbefore " . $ret . "\n"}
 	    }
 	    case "x-start-after" {
-		my $ret = l2of $lsbinfo{$key};
+		my $ret = l2of $lsbinfo->{$key};
 		if ($ret) {$dep .= "\tafter " . $ret . "\n"}
 	    }
 	}
     }
 }
+
+my $rst;
+
 if ($dep) {$rst = "depend () {\n" . $dep . "}\n"};
 if ($des) {$rst = $des . "\n" . $rst};
 print $rst
